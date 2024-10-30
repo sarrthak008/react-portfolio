9f5c6eefde3059979953ba67519b0de2bcbde0da
sarrthak008 <sarthaknavale08@gmail.com>
2024-10-30 12:46:02 +0530

    add data on a p

--- src/components/FileOpener.jsx
+++ src/components/FileOpener.jsx
@@ -2,11 +2,19 @@ import React from 'react'
 import Nav from './Nav'
 
 const FileOpener = ({data}) => {
+
+
   return (
     <div className='h-lvh w-lvw bg-[#282828] absolute top-0 left-0 z-10'>
       <Nav/>
-      <div>
-         
+      <span className='mt-9 ml-1 block text-yellow-100 text-xl font-semibold opacity-70'>📄 {data.name}</span>
+      <div className='w-[90%] bg-[#32312F] h-[80%] m-auto overflow-y-scroll text-gray-200 mt-4 px-3'>
+         {data.fildData.map((showdata)=>{
+           return(<>
+                <h2 className='font-bold text-4xl'>{showdata.title}</h2>
+                <p className=' text-left text-[1.1rem] leading-6 mt-6'>{showdata.discription}</p>
+            </>)
+         })}
       </div>
  
     </div>
--- src/config/ShowData.js
+++ src/config/ShowData.js
@@ -2,14 +2,23 @@
 const FILES_DATA =[
      {
         type:'file',
-        name:'my story'
+        name:'My Story',
+        fildData:[
+          
+           {
+            title:"My Story",
+            discription : 'Hii 👋 , My name is sarthak Navale . I m fullStack webdeveloper from India . I enjoy learning new skills and keeping up with the latest trends in technology to deliver great projects.',
+            Image:false
+           }
+
+        ]
      },
      {
          type:'contact',
          name:''
      },{
         type:'folder',
-        name:'projectsssssssss'
+        name:'projects'
      }
 ]
 
